3c032b9b5ea45f6bd75edae3d302ba6da0c71d81
motii <108280115+motii8128@users.noreply.github.com>
2024-12-03 00:03:16 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,57 +1,134 @@
-import logo from './moti_auto.svg';
 import './App.css';
-import { useState } from 'react';
 import ROSLIB from 'roslib';
+import { useState } from 'react';
 
 const ros = new ROSLIB.Ros({
-  url : 'ws://192.168.11.65:9090'
+  url: 'ws://192.168.11.65:9090'
 });
-const topic = new ROSLIB.Topic({
-  ros : ros,
-  name : '/from_iphone',
-  messageType : 'std_msgs/Int32'
-})
 
+const sub_current = new ROSLIB.Topic({
+    ros : ros,
+    name : '/current',
+    messageType : 'geometry_msgs/Point'
+});
+
+const pub_target = new ROSLIB.Topic({
+    ros : ros,
+    name : '/target',
+    messageType : 'geometry_msgs/Point'
+});
+
+function CurrentView()
+{
+  const [cur_pos, setPosition] = useState({x:0.0, y:0.0, r : 0.0});
+
+  sub_current.subscribe((msg)=>{
+    setPosition(msg.x, msg.y, msg.z);
+  });
+
+  return(
+    <div>
+      <p style={{ fontSize: "48px" }}>Current Pose</p>
+      <p>x:{cur_pos.x}</p>
+      <p>y:{cur_pos.y}</p>
+      <p>rotation:{cur_pos.r}</p>
+    </div>
+  );
+}
+
+function TargetPublisher()
+{
+  const send = (tx, ty, tz) =>{
+    const msg = new ROSLIB.Message({
+        x : tx,
+        y : ty,
+        z : tz
+    });
+
+    pub_target.publish(msg);
+  };
+
+  const [x, SetX] = useState(0.0);
+  const [y, SetY] = useState(0.0);
+  const [z, SetZ] = useState(0.0);
+
+  const xHandler = (event) =>{
+    const inputValue = parseFloat(event.target.value);
+    if(!isNaN(inputValue))
+    {
+      SetX(inputValue);
+    }
+  };
+
+  const yHandler = (event) =>{
+    const inputValue = parseFloat(event.target.value);
+    if(!isNaN(inputValue))
+    {
+      SetY(inputValue);
+    }
+  };
+    
+  const zHandler = (event) =>{    
+    const inputValue = parseFloat(event.target.value);
+    if(!isNaN(inputValue))
+    {
+      SetZ(inputValue);
+    }
+  };
+
+  const Click = () => {
+      send(x, y, z);
+  }
+
+  return(
+    <div>
+      <p style={{ fontSize: "48px" }}>Target Pose</p>
+      <label htmlFor="xInput">x:</label>
+      <input
+          type="number"
+          id="xInput"
+          value={x}
+          onChange={xHandler}
+      />
+      <p></p>
+      <label htmlFor="yInput">y:</label>
+      <input
+          type="number"
+          id="yInput"
+          value={y}
+          onChange={yHandler}
+      />
+      <p></p>
+      <label htmlFor="zInput">rotation:</label>
+      <input
+          type="number"
+          id="zInput"
+          value={z}
+          onChange={zHandler}
+      />
+      <p></p>
+      <button onClick={Click} style={{
+          fontSize: '24px', 
+          padding: '40px 80px',
+          backgroundColor: '#4CAF50',
+          color: 'white',
+          border: 'none',
+          borderRadius: '5px',
+          cursor: 'pointer', }}>Publish</button>
+      </div>
+  );
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Button />
-        </p>
-        
+            <CurrentView />
+            <TargetPublisher />
       </header>
     </div>
   );
 }
 
-function Button()
-{
-  const send = (value) =>{
-    const msg = new ROSLIB.Message({
-      data : value
-    });
 
-    topic.publish(msg);
-  };
-
-  const [count, setCount] = useState(0);
-  const handleClick = () =>{
-    setCount(count + 1);
-    send(count);
-  }
-  
-  return(
-    <button onClick={handleClick} style={{
-       fontSize: '24px', 
-       padding: '40px 80px',
-       backgroundColor: '#4CAF50', // 背景色
-       color: 'white',            // テキストの色
-       border: 'none',            // ボーダーを非表示
-       borderRadius: '5px',       // 角を丸くする
-       cursor: 'pointer', }}>{count}</button>
-  );
-}
 
 export default App;
